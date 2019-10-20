82cc2503d2cf75fe6bda6243d2f0058d0ae5ae08
Risusagi <anastazja199707@gmail.com>
2019-10-20 17:47:41 +0200

    codemirror added

--- src/js/taskTemplate.js
+++ src/js/taskTemplate.js
@@ -1,1 +1,13 @@
 import '../style/taskTemplate.scss';
+import CodeMirror from './codemirror/lib/codemirror.js';
+import './codemirror/mode/javascript/javascript.js';
+import './codemirror/addon/scroll/simplescrollbars.js';
+
+
+const editorWindow = CodeMirror(document.querySelector('.editor'), {
+    mode: "javascript",
+    theme: "cobalt",
+    lineNumbers: true,
+    lineWrapping: true,
+    scrollbarStyle: "overlay"
+});
