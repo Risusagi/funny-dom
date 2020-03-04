f4fc5dd0de6104075ca4d5b6cccf607b95acd050
Risusagi <anastazja199707@gmail.com>
2020-03-04 09:01:07 +0100

    Fixed switching to taskTemplate in case user finished starting chat previously
    Changed path to switch to risusagi.github.io/funny-dom/taskTemplate not risusagi.github.io/taskTemplate

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import '../taskTemplate.html';
 const startPoint = localStorage.getItem('undoneChallenge');
 
 if (startPoint) {
-    window.open('../taskTemplate.html', '_self');
+    window.open('taskTemplate.html', '_self');
 } else {
     const startPage = new StartPage();
     startPage.startChat();
